7b0cd5752706490599cc8a36ba0219531c147ed4
Srinivas <srinivaspolishetty@gmail.com>
2020-03-19 18:53:33 +0100

    added i15N country codes
    added i15N country codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as i18nIsoCountries from 'i18n-iso-countries';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-translate-app';
+
+  ngOnInit() {
+    i18nIsoCountries.registerLocale(require("i18n-iso-countries/langs/en.json"));
+        let value = '';
+        value = i18nIsoCountries.getName('840', 'en');
+         console.log(value);
+        // value = i18nIsoCountries.getName('AL', 'en');
+        // console.log(value);
+        // console.log(i18nIsoCountries.getName("AL", "en"));
+        // console.log(i18nIsoCountries.getName("840","en"));
+        // console.log(i18nIsoCountries.getNames("en"));
+        // console.log(i18nIsoCountries.getAlpha2Codes());
+        
+      }
 }
